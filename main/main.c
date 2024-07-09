a46282500698149589cc8470b3261756f45a4716
지 호근 <jih@m2.local>
2024-07-09 20:52:35 +0900

    12 Components & BME680 Example

--- main/main.c
+++ main/main.c
@@ -1,25 +1,15 @@
 /**
- * Application entry point.
+ * Application entry point
  */
 
-#include "esp_log.h"
-#include "nvs_flash.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-#include "coding_style.h"
+#include "bme680_sensor.h"
 
 void app_main(void)
 {
-    // Initialize NVS
-	esp_err_t ret = nvs_flash_init();
-	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-	{
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		ret = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(ret);
-
-	// Example call to coding_style function
-	// coding_style_public(1, 2, 3); // 정상 실행
-	coding_style_public(1, 2, 4); // 에러 발생
+	// Start the BME680 task
+	BME680_task_start();
 }
-
