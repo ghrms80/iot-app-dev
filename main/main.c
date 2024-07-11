ae01f5bc6ca4acba88060fc9b3941bbc5d86d082
지 호근 <jih@m2.local>
2024-07-11 10:43:19 +0900

    7 RGB Application Status LED

--- main/main.c
+++ main/main.c
@@ -1,15 +1,25 @@
-/**
- * Application entry point
- */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+// #include "esp_wifi.h"
+// #include "esp_system.h"
+// #include "esp_event.h"
+// #include "esp_event_loop.h"
+// #include "nvs_flash.h"
+// #include "driver/gpio.h"
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-
-#include "bme680_sensor.h"
+#include "rgb_led.h"
 
 void app_main(void)
 {
-	// Start the BME680 task
-	BME680_task_start();
+    while (true)
+    {
+        rgb_led_wifi_app_started();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        rgb_led_http_server_started();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        rgb_led_wifi_connected();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
